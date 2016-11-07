8f5c09ef51cf18df898b4f264d3a3b4f30a0b71d
mayiwei <mayiwei889@126.com>
2016-11-07 22:19:13 +0800

    add search

--- app/src/main/java/com/kachidoki/myselfview2/MainActivity.java
+++ app/src/main/java/com/kachidoki/myselfview2/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    SearchView view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = new SearchView(getApplicationContext());
     }
 }
